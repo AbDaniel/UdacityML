e1973bba8a8ae279dcaeea070c8d54232723b8bf
daniel <abrahamw@thoughtworks.com>
2016-03-12 21:56:19 +0530

    Added Decision Tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -9,8 +9,10 @@
 """
     
 import sys
-from time import time
+import numpy
+from timeit import default_timer as timer
 sys.path.append("../tools/")
+from sklearn import tree
 from email_preprocess import preprocess
 
 
@@ -24,6 +26,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+start = timer()
+clf.fit(features_train, labels_train)
+end = timer()
+print "Fitting time {0}".format(end - start)
+
+start = timer()
+predictions = clf.predict(features_test)
+end = timer()
+print "Prediction time {0}".format(end - start)
+
+print "No of Chris Mails: {0}".format(len(numpy.where(predictions == 1)[0]))
+
+score = clf.score(features_test, labels_test)
+print "Score {0}".format(score)
+
 
 
 #########################################################
