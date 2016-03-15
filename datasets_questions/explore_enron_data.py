3839d4bd56872a33850dd426ebc1da8b430c6e54
daniel <abrahamw@thoughtworks.com>
2016-03-15 09:43:28 +0530

    Added Answers for enron dataset questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,25 @@
 """
 
 import pickle
+import pandas as pd
+import numpy as np
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+df = pd.DataFrame(enron_data).T
 
+# print len(df[df['poi'] == True])
+print df.columns
+
+print df.loc['PRENTICE JAMES']['total_stock_value']
+print df.loc['COLWELL WESLEY']['from_this_person_to_poi']
+print df.loc['SKILLING JEFFREY K']['exercised_stock_options']
+print df.loc['SKILLING JEFFREY K']['total_payments']
+print df.loc['LAY KENNETH L']['total_payments']
+print df.loc['FASTOW ANDREW S']['total_payments']
+print len(df['salary'][df['salary'] != 'NaN'])
+print len(df['email_address'][df['email_address'] != 'NaN'])
+print len(df['total_payments'][df['total_payments'] != 'NaN'])
+print df['total_payments'][df['total_payments'] != 'NaN']
+print len(df[ (df['poi'] == True) & (df['total_payments'] != 'NaN') ])
+print len(df[ (df['poi'] == True) ])
